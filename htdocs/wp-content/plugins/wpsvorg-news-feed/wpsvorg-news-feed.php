95b5a67db674c1630d4a615e80ea27788f0d7208
Stanislav Khromov <stanislav.khromov@schibsted.se>
2018-04-25 17:17:21 +0200

    Add special news feed plugin

---
+++ htdocs/wp-content/plugins/wpsvorg-news-feed/wpsvorg-news-feed.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Special news feed for sv.wordpress.org
+Plugin URI: http://wordpress.org/extend/plugins/wp-piwik/
+Version: 1.0
+*/
+
+add_action('init', function() {
+	if(isset($_GET['wpsvorg_feed'])) {
+
+
+		if(!$result = get_transient('wpsvorg_feed')) {
+
+			$feedObject = simplexml_load_file('https://sv.wordpress.org/feed/');
+
+			if(isset($feedObject->channel->item[0])) {
+				unset($feedObject->channel->item[0]);
+			}
+			$result = $feedObject->asXML();
+			set_transient('wpsvorg_feed', $result, 3600);
+		}
+
+		header('Content-Type: application/rss+xml; charset=UTF-8');
+		header('Cache-Control: no-cache, must-revalidate, max-age=0');
+		echo $result;
+		die();
+	}
+});
